a4c777eaab67a67ce8a98c3f5bf1afb71c854426
eschelkunov <eschelkunov@gmail.com>
2019-06-08 15:26:42 +0300

    git config core.ignorecase false

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const sequelize = require('../config/dbconnection');
+
+  module.exports = sequelize.define('Post', {
+    id: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+  
+    post_title: Sequelize.STRING(255),
+    
+    post_content: Sequelize.STRING(300),
+  
+    createdAt: Sequelize.DATE,
+  
+    updatedAt: Sequelize.DATE,
+    
+    user_id: Sequelize.INTEGER(11)
+
+  });
+  
