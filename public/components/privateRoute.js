f4e246657dcb0c12febf132ef7893d98c321d931
eschelkunov <eschelkunov@gmail.com>
2019-10-23 12:05:16 +0300

    removed debugger

--- public/components/privateRoute.js
+++ public/components/privateRoute.js
@@ -4,7 +4,6 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
-  debugger;
   return (
     <Route
       {...rest}
